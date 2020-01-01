5c778150bfb916bfbbd1b868b119bb1a7fdf54d5
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-01-01 13:45:29 +0300

    Correct test functions

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -4,8 +4,6 @@ test.each([
   ['good case', 'Willie', 'Swordsman', {
     name: 'Willie', type: 'Swordsman', health: 100, level: 1, attack: 40, defense: 10,
   }],
-  ['bad name', 'Willie!!!!!!!!!!', 'Swordsman', {}],
-  ['bad type', 'Willie', 'Snowman', {}],
 ])(
   ('should calculate %s'),
   (userCase, name, type, expected) => {
@@ -14,3 +12,11 @@ test.each([
     expect(result).toEqual(expected);
   },
 );
+
+test.each([
+  ['bad name', 'Willie!!!!!!!!!!', 'Swordsman', 'Incorrect name length'],
+  ['bad type', 'Willie', 'Snowman', 'Incorrect type of character'],
+])('trying to test errors', (errorCase, name, type, expected) => {
+  console.log(`Case: ${errorCase}`);
+  expect(Character.bind(this, name, type)).toThrow(expected);
+});
--- src/js/character.js
+++ src/js/character.js
@@ -1,27 +1,21 @@
 export default function Character(name, type) {
-  try {
-    if (name.length < 2 || name.length > 10) throw new Error('Incorrect name length');
+  if (name.length < 2 || name.length > 10) throw new Error('Incorrect name length');
 
-    const allTypes = [
-      { heroType: 'Bowman', attack: 25, defense: 25 },
-      { heroType: 'Swordsman', attack: 40, defense: 10 },
-      { heroType: 'Magician', attack: 10, defense: 40 },
-      { heroType: 'Undead', attack: 25, defense: 25 },
-      { heroType: 'Zombie', attack: 40, defense: 10 },
-      { heroType: 'Daemon', attack: 10, defense: 40 },
-    ];
+  const allTypes = [
+    { heroType: 'Swordsman', attack: 40, defense: 10 },
+    { heroType: 'Magician', attack: 10, defense: 40 },
+    { heroType: 'Undead', attack: 25, defense: 25 },
+    { heroType: 'Zombie', attack: 40, defense: 10 },
+    { heroType: 'Daemon', attack: 10, defense: 40 },
+    { heroType: 'Bowman', attack: 25, defense: 25 },
+  ];
 
-    const checkingType = allTypes.find((item) => item.heroType === type);
-
-    if (checkingType === undefined) throw new Error('Incorrect type of character');
-
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = checkingType.attack;
-    this.defense = checkingType.defense;
-  } catch (error) {
-    console.log('We\'ve got Error');
-  }
+  const checkingType = allTypes.find((item) => item.heroType === type);
+  if (checkingType === undefined) throw new Error('Incorrect type of character');
+  this.name = name;
+  this.health = 100;
+  this.level = 1;
+  this.attack = checkingType.attack;
+  this.defense = checkingType.defense;
+  this.type = type;
 }
